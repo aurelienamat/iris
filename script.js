d5e4300baeaa892e5332bc3a9200e5461a684c11
aurelienamat <aurelien.amat@sfr.fr>
2026-04-10 12:00:24 +0200

    Route connexion inscription

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
-const socket = new WebSocket('ws://localhost:3003');
+// ROUTES ---------------------------------------------------------------------------------------------------------------------------------------------------------
 
-socket.onopen = () => {
-    console.log('Connecté au serveur');
-    socket.send('Bonjour serveur !');
-};
+// ROUTE INSCRIPTION ========================================================
+// envoie du username/password en POST sur /inscription
+
+//Variables
+const password = document.getElementById('passwordInput');
+const username = document.getElementById('usernameInput');
+
+btnInscription.addEventListener('click', () => {
+
+    fetch('/inscription', {
+        credentials: 'include',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nom: username.value,
+            password: password.value,
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.erreur) {
+                alert(data.erreur.sqlMessage); // Ex: "Mot de passe invalide" ou "Inscription reussie !"
+            } else {
+                alert(data.message);
+            }
+            console.log(data);
+        });
+});
+
+// ROUTE CONNEXION ====================================================================
+// envoie username/password en POST sur /connexion
+// le serveur renvoie l'id de l'utilisateur
+
+connexion.addEventListener('click', () => {
+    fetch('/connexion', {
+        credentials: 'include',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username.value,
+            password: password.value
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.message == 'connexion echoué') { // Connexion échouée
+                alert(data.message);
+                console.log(data.message);
+            } else { // Connexion réussie, on sauvegarde la classe dans le localStorage
+                //console.log('Connexion réussie, classe : ' + data.classe);
+               
+                //Remplissage du local storage
+                localStorage.setItem('idUsers', data.idUsers);
+                localStorage.setItem('username', data.username);
+            }
+        })
+})
